6011fcc61bf7ef661561f73b0b06b8f9f473ab3f
Snork712 <snir712@gmail.com>
2022-04-30 04:31:44 +0300

    first

---
+++ javascript/game.js
@@ -0,0 +1,168 @@
+let pickaxe = document.querySelector("#pickaxe");
+let shovel = document.querySelector("#shovel");
+let axe = document.querySelector("#axe");
+let grid = document.querySelector("#grid");
+let inventory = document.querySelector("#inventory");
+let currentElementBox = document.querySelector("#currentElementBox");
+
+let currentTool = "none";
+let currentElement = "empty";
+
+pickaxe.addEventListener("click", () => {
+  currentTool = "pickaxe";
+});
+shovel.addEventListener("click", () => {
+  currentTool = "shovel";
+});
+axe.addEventListener("click", () => {
+  currentTool = "axe";
+});
+currentElementBox.addEventListener("click", () => {
+  currentTool = "box";
+});
+grid.addEventListener("click", () => {
+  currentTool = "none";
+  currentElementBox.style.borderColor = "black";
+  shovel.style.backgroundColor = "rgb(174, 171, 168)";
+  pickaxe.style.backgroundColor = "rgb(174, 171, 168)";
+  axe.style.backgroundColor = "rgb(174, 171, 168)";
+});
+
+inventory.addEventListener("click", () => {
+  switch (currentTool) {
+    case "pickaxe":
+      pickaxe.style.backgroundColor = "rgb(171, 158, 144)";
+      shovel.style.backgroundColor = "rgb(174, 171, 168)";
+      axe.style.backgroundColor = "rgb(174, 171, 168)";
+      currentElementBox.style.borderColor = "black";
+      break;
+    case "shovel":
+      shovel.style.backgroundColor = "rgb(171, 158, 144)";
+      pickaxe.style.backgroundColor = "rgb(174, 171, 168)";
+      axe.style.backgroundColor = "rgb(174, 171, 168)";
+      currentElementBox.style.borderColor = "black";
+      break;
+    case "axe":
+      axe.style.backgroundColor = "rgb(171, 158, 144)";
+      pickaxe.style.backgroundColor = "rgb(174, 171, 168)";
+      shovel.style.backgroundColor = "rgb(174, 171, 168)";
+      currentElementBox.style.borderColor = "black";
+      break;
+    case "box":
+      axe.style.backgroundColor = "rgb(174, 171, 168)";
+      pickaxe.style.backgroundColor = "rgb(174, 171, 168)";
+      shovel.style.backgroundColor = "rgb(174, 171, 168)";
+      currentElementBox.style.borderColor = "white";
+      break;
+  }
+});
+
+// grid
+// grid
+// grid
+// grid
+// grid
+// grid
+
+const d = "dirt";
+const s = "stone";
+const w = "wood";
+const l = "leave";
+const c = "clowd empty";
+const e = "empty";
+let rows = 22;
+let columns = 22;
+let board = [
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e],
+  [e, e, e, e, c, c, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e],
+  [e, e, e, c, c, c, c, e, e, e, e, e, e, e, e, e, e, e, e, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, l, l, l, l, l, l, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, l, l, l, l, l, l, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, l, l, l, l, l, l, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, l, l, l, l, l, l, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, l, l, l, l, l, l, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, w, w, e, e, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, w, w, e, e, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, w, w, e, e, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, w, w, e, e, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, w, w, e, e, e, e],
+  [e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, w, w, e, e, e, e],
+  [d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d],
+  [d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d],
+  [d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d],
+  [d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d],
+  [s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s],
+  [s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s],
+];
+
+function paintBoard() {
+  for (let r = 1; r < rows; r++) {
+    for (let c = 1; c < columns; c++) {
+      let block = document.createElement("div");
+      block.style.gridRowStart = r;
+      block.style.gridColumnStart = c;
+      block.classList = board[r - 1][c - 1];
+      grid.append(block);
+    }
+  }
+}
+paintBoard();
+// switch
+// switch
+// switch
+// switch
+// switch
+
+function del(element, tool) {
+  let block = document.querySelectorAll("." + element);
+  for (let i = 0; i < block.length; i++) {
+    let b = block[i];
+    b.addEventListener("click", () => {
+      if (currentTool === tool && currentElement === "empty") {
+        let r = b.style.gridRowStart;
+        let c = b.style.gridColumnStart;
+        board[r - 1][c - 1] = e;
+        b.classList = board[r - 1][c - 1];
+        currentTool = "none";
+        currentElement = element;
+        currentElementBox.classList = currentElement;
+      }
+    });
+  }
+}
+function add() {
+  if (currentElement !== "empty") {
+    let block = document.querySelectorAll(".empty");
+    for (let i = 0; i < block.length; i++) {
+      const b = block[i];
+      b.addEventListener("click", (e) => {
+        if (currentTool === "box" && currentElement !== "empty") {
+          let r = b.style.gridRowStart;
+          let c = b.style.gridColumnStart;
+          board[r - 1][c - 1] = currentElement;
+          b.classList = board[r - 1][c - 1];
+          currentTool = "none";
+          currentElement = "empty";
+          currentElementBox.classList = currentElement;
+        }
+      });
+    }
+  }
+}
+inventory.addEventListener("click", () => {
+  switch (currentTool) {
+    case "pickaxe":
+      del("stone", "pickaxe");
+      break;
+    case "shovel":
+      del("dirt", "shovel");
+      break;
+    case "axe":
+      del("wood", "axe");
+      del("leave", "axe");
+      break;
+    case "box":
+      add();
+  }
+});
---
+++ javascript/inventory.js
@@ -0,0 +1,34 @@
+// let pickaxe = document.querySelector("#pickaxe");
+// let shovel = document.querySelector("#shovel");
+// let axe = document.querySelector("#axe");
+
+// export let currentTool = "";
+
+// pickaxe.addEventListener("click", () => {
+//   currentTool = "pickaxe";
+// });
+// shovel.addEventListener("click", () => {
+//   currentTool = "shovel";
+// });
+// axe.addEventListener("click", () => {
+//   currentTool = "axe";
+// });
+// inventory.addEventListener("click", () => {
+//   switch (currentTool) {
+//     case "pickaxe":
+//       pickaxe.style.backgroundColor = "blue";
+//       shovel.style.backgroundColor = "bisque";
+//       axe.style.backgroundColor = "bisque";
+//       break;
+//     case "shovel":
+//       shovel.style.backgroundColor = "blue";
+//       pickaxe.style.backgroundColor = "bisque";
+//       axe.style.backgroundColor = "bisque";
+//       break;
+//     case "axe":
+//       axe.style.backgroundColor = "blue";
+//       pickaxe.style.backgroundColor = "bisque";
+//       shovel.style.backgroundColor = "bisque";
+//       break;
+//   }
+// });
